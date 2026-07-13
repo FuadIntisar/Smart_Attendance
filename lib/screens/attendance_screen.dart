d71688d998739916ee5df499b12a827db9718a5f
intisarfuad3@gmail.com <intisarfuad3@gamil.com>
2026-07-14 00:12:34 +0600

    Create attendance screen

--- lib/screens/attendance_screen.dart
+++ lib/screens/attendance_screen.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-class AttendanceScreen extends StatelessWidget {
+class AttendanceScreen extends StatefulWidget {
   const AttendanceScreen({super.key});
 
   @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  final TextEditingController studentIdController = TextEditingController();
+
+  
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Mark Attendance"),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          "Attendance Screen",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: AppBar(title: const Text("Mark Attendance"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Student ID",
+              style: TextStyle(
+                fontSize: 18, 
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            TextField(
+              controller: studentIdController,
+              decoration: const InputDecoration(
+                hintText: "Enter Student ID",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
         ),
       ),
     );
