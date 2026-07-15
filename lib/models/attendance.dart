b349de869a6e906a36f595a2ab1734c35b9d7230
intisarfuad3@gmail.com <intisarfuad3@gamil.com>
2026-07-15 19:46:34 +0600

    Add attendance model

---
+++ lib/models/attendance.dart
@@ -0,0 +1,13 @@
+
+
+class Attendance {
+  final String studentId;
+  final String course;
+  final String section;
+
+  Attendance({
+    required this.studentId,
+    required this.course,
+    required this.section,
+  });
+}
